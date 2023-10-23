235fc4dafacfee1452b7899a7ed8373de9900357
Daniel <danielinfomlg@gmail.com>
2023-10-23 11:29:37 +0200

    registro de ficheros

---
+++ estaticos/js/funciones.js
@@ -0,0 +1,55 @@
+const circulos = document.querySelectorAll(".circulo");
+const contenedorTiempo = document.querySelector(".tiempo");
+const textoModal = document.querySelector(".modal h1");
+const botonJugar = document.querySelector(".modal button");
+const modal = document.querySelector(".modal");
+let puntuacion = 0;
+let contador = null;
+let tiempo = 8;
+let estadoJuego = false; // true --> he ganado | false --> he perdido o es la primera partida
+
+function juego(tiempo){   // Esta es la parte referida al tiempo-contador
+    puntuacion = 0;
+    contenedorTiempo.innerHTML = "";
+    circulos.forEach(circulo => circulo.classList.remove("invisible"));
+    for(let i = 0; i < tiempo; i++){
+        let bloque = document.createElement("div");
+        bloque.style.width = `calc(${100/tiempo}% - 10px)`;
+        contenedorTiempo.appendChild(bloque);
+    }
+
+    contador = setInterval(() => {
+        contenedorTiempo.children[0].remove();
+        if(contenedorTiempo.children.length == 0){
+            clearInterval(contador);
+            estadoJuego = false;
+            textoModal.innerHTML = "has perdido";
+            modal.classList.add("visible");
+        }
+    },1000); 
+}
+
+
+// Esta de abajo es la parte referida a los círculos y como funcionan haciendoles click y hasta cuando podemos jugar "12 clicks"
+
+for(let i = 0; i < circulos.length; i++){
+    circulos[i].addEventListener("click", () => {
+        circulos[i].classList.add("invisible");
+        puntuacion++;
+        console.log(puntuacion);
+        if(puntuacion == 12){
+            clearInterval(contador);
+            estadoJuego = true;
+            textoModal.innerHTML = "has ganado";
+            modal.classList.add("visible");
+        }
+    });
+}
+
+
+botonJugar.addEventListener("click", () => {
+    modal.classList.remove("visible");
+    juego(estadoJuego ? --tiempo : tiempo);
+});
+
+
