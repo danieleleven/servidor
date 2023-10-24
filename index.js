07ccd9236ccefac06379d85e9aadbc472d248208
Daniel <danielinfomlg@gmail.com>
2023-10-24 23:47:34 +0200

    Servicio web color

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ const {tipo,servirFichero} = require(path.join(__dirname,"funciones.js"));
 let puerto = process.env.PORT || 3000;
 
 
+
 const servidor = http.createServer((peticion,respuesta) => {
     
     if(peticion.url == "/"){
         servirFichero(respuesta,path.join(__dirname,"estaticos/index.html"),tipo("html"),200);
+
+    }else if(peticion.url == "/color"){  
+        
+        respuesta.writeHead(200,{ "Content-type" : "application/json"});
+
+        let [r,g,b] = [0,0,0].map(() => Math.floor(Math.random() * 256));
+
+        respuesta.write(JSON.stringify({r,g,b}));
+
+        respuesta.end();
+
+        
     }else{
         let ruta = path.join(__dirname,"estaticos",peticion.url);
 
